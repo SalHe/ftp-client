b82ae83fbc518072e4aab847f065e9048c4947d1
SalHe <SalHe@qq.com>
2021-08-03 21:20:41 +0800

    √连接/断开的Command绑定

--- FTPClient.GUI/MainViewModel.cs
+++ FTPClient.GUI/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
 using ReactiveUI;
@@ -9,6 +10,8 @@ namespace FTPClient.GUI
 {
     public class MainViewModel : ReactiveObject
     {
+        #region Properties
+
         private string _host = "127.0.0.1";
         private int _port = 21;
         private string _username = "admin";
@@ -20,31 +23,55 @@ namespace FTPClient.GUI
             get => _host;
             set => this.RaiseAndSetIfChanged(ref _host, value);
         }
-
         public int Port
         {
             get => _port;
             set => this.RaiseAndSetIfChanged(ref _port, value);
         }
-
         public string Username
         {
             get => _username;
             set => this.RaiseAndSetIfChanged(ref _username, value);
         }
-
         public string Password
         {
             get => _password;
             set => this.RaiseAndSetIfChanged(ref _password, value);
         }
-
         public bool Connected
         {
             get => _connected;
-            set => this.RaiseAndSetIfChanged(ref _connected, value);
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _connected, value);
+                this.RaisePropertyChanged(nameof(NotConnected));
+            }
         }
 
         public bool NotConnected => !Connected;
+
+        #endregion
+
+        #region Commands
+
+        public ReactiveCommand<Unit, Unit> ConnectFtpServerCommand { get; }
+        public ReactiveCommand<Unit, Unit> DisconnectFromFtpServeCommand { get; }
+
+        #endregion
+
+        public MainViewModel()
+        {
+            ConnectFtpServerCommand = ReactiveCommand.CreateFromTask(async () =>
+            {
+                // TODO 实现连接到服务器
+                Connected = true;
+            });
+
+            DisconnectFromFtpServeCommand = ReactiveCommand.CreateFromTask(async () =>
+            {
+                // TODO 实现断开连接
+                Connected = false;
+            });
+        }
     }
 }

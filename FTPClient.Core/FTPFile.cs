f76a74195be24e03ce3f3e3b275d0011e17824d9
SalHe <SalHe@qq.com>
2021-08-05 00:02:17 +0800

    加入文件信息解析方法

--- FTPClient.Core/FTPFile.cs
+++ FTPClient.Core/FTPFile.cs
@@ -38,9 +38,53 @@ namespace FTPClient.Core
         /// </summary>
         public string OwnerGroup { get; set; }
 
+        public bool IsDirectory => Grants.StartsWith("d");
+
         public override string ToString()
         {
             return $"{nameof(Grants)}: {Grants}, {nameof(Size)}: {Size}, {nameof(Time)}: {Time}, {nameof(Owner)}: {Owner}, {nameof(OwnerGroup)}: {OwnerGroup}";
+        }
+
+        /// <summary>
+        /// 从FTP服务器LIST命令返回的一行数据中解析出对应的文件信息。
+        /// 
+        /// </summary>
+        /// <param name="os">服务器操作系统类型</param>
+        /// <param name="line">一行信息</param>
+        /// <param name="baseDir">所在目录完整路径</param>
+        /// <returns>对应的FTPFile实例</returns>
+        public static FTPFile FromFTPListLine(OperationSystemType os, string line, string baseDir)
+        {
+            FTPFile file = null;
+            if (os == OperationSystemType.Linux || os == OperationSystemType.Unix)
+            {
+                file = new FTPFile()
+                {
+                    Grants = line.Substring(0, 10),
+                    Owner = line.Substring(16, 8),
+                    OwnerGroup = line.Substring(26, 8),
+                    Size = int.Parse(line.Substring(34, 8)),
+                    Time = DateTime.Parse(line.Substring(43, 12)),
+                    FilePath = baseDir + "/" + line.Substring(56)
+                };
+                if (file.IsDirectory) file.Size = -1;
+                return file;
+            }
+            else if (os == OperationSystemType.Windows)
+            {
+                file = new FTPFile()
+                {
+                    Grants = line.Substring(0, 10),
+                    Owner = line.Substring(13, 3),
+                    OwnerGroup = line.Substring(13, 3),
+                    Size = int.Parse(line.Substring(21, 14)),
+                    Time = DateTime.Parse(line.Substring(36, 12)),
+                    FilePath = baseDir + "/" + line.Substring(49)
+                };
+                if (file.IsDirectory) file.Size = -1;
+                return file;
+            }
+            throw new ArgumentException("无效的操作系统类型");
         }
     }
 }
